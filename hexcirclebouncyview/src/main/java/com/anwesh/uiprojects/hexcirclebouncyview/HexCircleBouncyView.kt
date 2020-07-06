d134b2234b92ea0951374429dcc59af8cc8b5507
Anwesh43 <anweshthecool0@gmail.com>
2020-07-07 01:59:13 +0530

    adding constants and libraries

--- hexcirclebouncyview/src/main/java/com/anwesh/uiprojects/hexcirclebouncyview/HexCircleBouncyView.kt
+++ hexcirclebouncyview/src/main/java/com/anwesh/uiprojects/hexcirclebouncyview/HexCircleBouncyView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.hexcirclebouncyview
 /**
  * Created by anweshmishra on 07/07/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val balls : Int = 6
+val parts : Int = 2
+val scGap : Float = 0.02f / parts
+val sizeFactor : Float = 2.9f
+val ballRFactor : Int = 5
+val ballSFactor : Int = 3
+val foreColor : Int = Color.parseColor("#4CAF50")
