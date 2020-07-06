0486aee9038821e54fd43cd2af2d918c61403c38
Anwesh43 <anweshthecool0@gmail.com>
2020-07-07 02:10:37 +0530

    adding HexCircleBouncyView class

--- hexcirclebouncyview/src/main/java/com/anwesh/uiprojects/hexcirclebouncyview/HexCircleBouncyView.kt
+++ hexcirclebouncyview/src/main/java/com/anwesh/uiprojects/hexcirclebouncyview/HexCircleBouncyView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawHCBNode(i : Int, scale : Float, paint : Paint) {
     drawHexBouncyCircle(scale, size, paint)
     restore()
 }
+
+class HexCircleBouncyView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
